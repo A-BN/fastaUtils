58027ccc58a05d16124d7aebf1e9c2ce38589650
Antoine Bridier-Nahmias <antoine.Bridier-Nahmias@inserm.fr>
2018-07-05 17:37:54 +0200

    fastanalyze() now returns a list containing the requested data

--- R/fastanalyze.R
+++ R/fastanalyze.R
@@ -1,30 +1,38 @@
 #' fastanalyze() gives information about a fasta file
-#' @description it counts the number of contigs, their size and the total size.
-#'   It also calculates the N50 / L50, the N90 / L90 and prints a synthetic plot
-#'   suming up all these results
-#' @param fasta_file path of the fasta file
-#' @param metrics boolean, indicates whether the metrics should be printed
+#' @description It counts the number of contigs, their size and the total size.
+#'   It also calculates the N50 / L50, the N90 / L90 and can print a synthetic
+#'   plot suming up all these results nicely.
+#' @param fasta_file path of the fasta file to analyze
+#' @param metrics boolean, indicates whether the metrics should be returned.
+#'   default: TRUE
+#' @param contigs boolean, indicates whether each contig name and size be
+#' returned. default: TRUE
 #' @param plot boolean, indicates whether a plot should be generated
-#' @param verbose boolean, indicates whether each contig name and size be
-#'   reported. default: TRUE
-#' @return Whatever you want
+#'   returned. default: TRUE
+#' @return metrics contigs and plot if requested.
 #' @export
 fastanalyze <-
-  function(fasta_file, metrics = TRUE, plot = TRUE, verbose = TRUE){
+  function(fasta_file, metrics = TRUE, contigs = TRUE, plot = TRUE, out = ""){
+
+    to_return <- list(contigs = "", contigs_metrics = "", plot = "")
     contigs_df <- read_fasta(fasta = fasta_file)
     contigs_metrics <- nx_lx(contig_df = contigs_df)
-    if(verbose){
-      print(contigs_df)
+
+    if(contigs){
+      to_return[["contigs"]] <- contigs_metrics[[1]]
     }
     if(metrics){
-      print(contigs_metrics)
+      to_return[["contigs_metrics"]] <- contigs_metrics[[2]]
     }
     if(plot){
       contigs_plot <- plot_contigs(contig_df = contigs_metrics[[1]], contigs_metrics[[2]])
       plot_name <- basename(fasta_file)
-      message("saving file ", plot_name, ".jpg")
-      ggplot2::ggsave(plot = contigs_plot, filename = paste0("./",plot_name, ".jpg"),device = "jpg")
+
+      to_return[[plot]] <- contigs_plot
+      if(out != ""){
+        message("saving file ", plot_name, ".jpg")
+        ggplot2::ggsave(plot = contigs_plot, filename = paste0(out, "/", plot_name, ".jpg"),device = "jpg")
+      }
     }
-    
-    return(contigs_metrics)
+    return(to_return)
 }
